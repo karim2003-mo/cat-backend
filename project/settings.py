c181e9d669e19e08980f96aeca5326a77c471023
Karim Gamal Elsayed <kareemabolisa@gmail.com>
2025-03-17 06:15:42 +0200

    v1.0.3

--- project/settings.py
+++ project/settings.py
@@ -35,7 +35,7 @@ CORS_ALLOWED_ORIGINS = [
 
 INSTALLED_APPS = [
     'app',
-    # 'corsheaders',
+    'corsheaders',
     'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -48,7 +48,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    # 'corsheaders.middleware.CorsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
