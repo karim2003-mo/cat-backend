6a9076566918bf0eec7d0599b0bd5adb49094000
Karim Gamal Elsayed <kareemabolisa@gmail.com>
2025-03-19 00:58:53 +0200

    vx3

--- app/models.py
+++ app/models.py
@@ -10,7 +10,7 @@ class WhatsUsers(models.Model):
     islatestmessagereaded=models.BooleanField(default=True)
     image=models.ImageField(upload_to="users_images/")
     lastactivity=models.IntegerField(default=12)
-    lastactivity=models.IntegerField(default=12)
+    lastcall=models.IntegerField(default=12)
     calltype=models.CharField(choices=call,max_length=10,default="voice")
     iscall=models.BooleanField(default=False)
     def __str__(self):
